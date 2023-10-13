bb0ce366a0e5c239085c66702b7d693afee7f7de
rplewi <rlewi0506@gmail.com>
2023-10-12 19:26:29 -0500

    Added buttons and text, JS was also added

---
+++ Matchmaker.js
@@ -0,0 +1,7 @@
+console.log("Matchmaker Starting...");
+
+function compatibilityCalculator(){
+    console.log("compatibilityCalculator");
+
+    document.getElementById("finalNumber").innerHTML = "Your Compatibility Is... (change Later)";
+}
